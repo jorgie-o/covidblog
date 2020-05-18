d6289faf1cb3de30ac289e5dc9f2cce8be0458dc
jeorge <georgeopuch.go@gmail.com>
2020-05-18 03:08:44 +0300

    routes and controller patches

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{config('app.name', 'covidblog')}}</title>
-        <h1>About</h1>
+        <h1>Home</h1>
 
     </head>
     <body>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
 
-Route::get('/about', function () {
-    return view('pages.about');
-});
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
