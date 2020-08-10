bfd6a4d313ae313e35341b2fde88413cb3ad32a7
jeorge <georgeopuch.go@gmail.com>
2020-08-10 18:03:01 +0300

    adds user story

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,22 @@
+ <nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle-collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">{{ config('app.name', 'covidblog') }}</a> 
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>        
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+  
